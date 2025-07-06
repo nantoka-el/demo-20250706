8553a150248ea2c837ae3a1e695fe244ec84394a
nantoka-el <fanky.kosuke@me.com>
2025-07-06 12:06:59 +0900

    Initialize React app with Tailwind and GitHub Pages workflow

---
+++ karaoke-app/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello Karaoke!</h1>
+    </div>
+  );
+}
+
+export default App;
